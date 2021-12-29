bf01aef515449ac4c765f144ed2bf90dad68cba1
fernando <fernando88to@gmail.com>
2021-12-29 14:38:26 -0300

    teste

--- src/integration-test/resources/GebConfig.groovy
+++ src/integration-test/resources/GebConfig.groovy
@@ -1,13 +1,22 @@
 import org.openqa.selenium.chrome.ChromeDriver
 import org.openqa.selenium.chrome.ChromeOptions
 import org.openqa.selenium.firefox.FirefoxDriver
+import org.openqa.selenium.remote.DesiredCapabilities
+import org.openqa.selenium.remote.RemoteWebDriver
 
 environments {
 
+    remote {
+        driver = {
+            def remoteWebDriverServerUrl = new URL("http://172.28.0.3:4444")
+            new RemoteWebDriver(remoteWebDriverServerUrl, DesiredCapabilities.firefox())
+        }
+    }
     // run via “./gradlew -Dgeb.env=chrome iT”
     chrome {
         driver = { new ChromeDriver() }
     }
+
 
     // run via “./gradlew -Dgeb.env=chromeHeadless iT”
     chromeHeadless {
