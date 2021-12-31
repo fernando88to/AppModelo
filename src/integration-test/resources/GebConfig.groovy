18a62ba0e65aea504b5140abdc2fe348ad5f4f09
fernando <fernando88to@gmail.com>
2021-12-31 14:13:02 -0300

    funcinando o geb com remote grid e rede em brigde

--- src/integration-test/resources/GebConfig.groovy
+++ src/integration-test/resources/GebConfig.groovy
@@ -1,53 +1,23 @@
-import com.google.common.io.BaseEncoding
 import org.openqa.selenium.chrome.ChromeDriver
 import org.openqa.selenium.chrome.ChromeOptions
 import org.openqa.selenium.firefox.FirefoxDriver
 import org.openqa.selenium.firefox.FirefoxOptions
-import org.openqa.selenium.firefox.FirefoxProfile
-import org.openqa.selenium.remote.DesiredCapabilities
 import org.openqa.selenium.remote.RemoteWebDriver
 
 environments {
 
     remote {
 
-
+        //dev is hostname of the machine where the code is running.
         baseUrl = "http://dev:8080/"
+
         driver = {
-
-
-           /* def remoteWebDriverServerUrl = new URL("http://127.0.0.1:4444/wd/hub")
-            def capabilities = new DesiredCapabilities()
-            capabilities.setBrowserName("firefox")
-            new RemoteWebDriver(remoteWebDriverServerUrl, capabilities)
-*/
-
+            def url = new URL("http://selenium-hub:4444")
 
 
             FirefoxOptions firefoxOptions = new FirefoxOptions();
             firefoxOptions.setPlatformName("Linux")
-            def url = new URL("http://selenium-hub:4444")
             new RemoteWebDriver(url, firefoxOptions);
-
-            /*def url = new URL("http://selenium-router:4444")
-            DesiredCapabilities caps = new DesiredCapabilities();
-            caps.setBrowserName("chrome");
-            new RemoteWebDriver(url,caps)*/
-
-            /*ChromeOptions chromeOptions = new ChromeOptions()
-            chromeOptions.setPlatformName('Linux')
-            //def url = new URL("http://selenium-hub:4444/wd/hub")
-            def url = new URL("http://172.26.0.6:5555/wd/hub")
-            new RemoteWebDriver(url, chromeOptions)*/
-
-            /*FirefoxOptions firefoxOptions = new FirefoxOptions();
-            def profile = new FirefoxProfile()
-            firefoxOptions.setProfile(profile);
-            firefoxOptions.setCapability("marionette", true);
-            def url = new URL("http://selenium-hub:4444/wd/hub")
-            new RemoteWebDriver(url, firefoxOptions);*/
-
-
         }
     }
     // run via “./gradlew -Dgeb.env=chrome iT”
